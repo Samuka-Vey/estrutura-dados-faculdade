0d289fd33106de0df28b99a3ade63d854e8df0c0
Marcos Samuel <marcosbarros@aluno.uespi.br>
2026-03-05 15:29:29 -0300

    Implementa a função principal para manipulação de arrays não ordenados

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from array_nao_ordenado import UnsortedArray as unsorted
 if __name__ == "__main__":
-    unsorted()
+    def dobro(x):
+        print(x * 2)
+    array = unsorted(10)
+    array.insert(1)
+    array.insert(2)
+    array.insert(3)
+    array.insert(4)
+    array.insert(5)
+    array.traverse(print)
+    
+    print(f"O elemento 3 está no índice: {array.find(3)}")
+    array.delete(2)
+    array.traverse(print)
+    
+    # dobro da lista
+    array.traverse(dobro)
+    array.traverse(print)    
