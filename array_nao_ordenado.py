7f16cdcf95cb1d473a508df30e1123b5c86c5846
Marcos Samuel <marcosbarros@aluno.uespi.br>
2026-03-05 17:45:58 +0000

    Adiciona classes Array e UnsortedArray para manipulação de arrays não ordenados

---
+++ array_nao_ordenado.py
@@ -0,0 +1,6 @@
+import core.Array as Array
+class UnsortedArray:
+    def __init__(self, max_size, typecode = 'l'):
+        self._array = Array(max_size, typecode)
+        self._max_size = max_size
+        self._size = 0
